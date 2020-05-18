bf17ded1ed70ee6e64cac9a952bc80a9eba5b591
fox-tracks <t.andriyanova@yahoo.com>
2020-05-18 13:44:00 +0300

    add 3 task

--- 01-javascript-data-types/3-omit/index.js
+++ 01-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const result = {};
+  const keys = Object.keys(obj);
 
+  keys.map(key => {
+    if([...fields].indexOf(key) === -1)
+      result[key] = obj[key];
+  })
+
+  return result;
 };
