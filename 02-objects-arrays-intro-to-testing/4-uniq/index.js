031c95947fe1b57a4f21f866efd3afc52cc05aba
fox-tracks <t.andriyanova@yahoo.com>
2020-05-21 19:20:34 +0300

    add task 4

--- 02-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 02-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,8 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  return (arr || []).reduce(function(acc, symbol) {
+    if(acc.indexOf(symbol) === -1) acc.push(symbol);
+    return acc;
+  }, []);
 }
