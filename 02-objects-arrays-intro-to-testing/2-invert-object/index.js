5cea64d3ba868a17351cd2c82e081208dd48da0d
fox-tracks <t.andriyanova@yahoo.com>
2020-05-21 17:41:16 +0300

    add task 2

--- 02-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 02-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if(obj === undefined) return undefined;
+  return Object.keys(obj).reduce(function(acc, key) {
+    (acc[obj[key]] = key);
+    return acc;
+  }, {})
 }
