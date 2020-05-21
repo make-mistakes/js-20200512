cfb3621006c255074d6c44ba4a16f6c004046e7d
fox-tracks <t.andriyanova@yahoo.com>
2020-05-21 21:32:24 +0300

    add task 3

--- 02-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 02-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) return string;
 
+  let result = '';
+  let count = 0;
+
+  if (size === 0) {
+    return result;
+  } else {
+    string.split('').reduce(function (prev, current) {
+      if (current !== prev) {
+        count = 1;
+        result += current;
+      } else {
+        if (count < size) {
+          count += 1;
+          result += current;
+        }
+      }
+
+      return current;
+    }, '')
+  }
+  return result;
 }
