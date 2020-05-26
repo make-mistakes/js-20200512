816771efe6e9522e4ca1c07e5f07f93609c22c00
fox-tracks <t.andriyanova@yahoo.com>
2020-05-26 10:41:34 +0300

    add notification class

--- 03-oop-basic-intro-to-dom/2-notification/src/index.js
+++ 03-oop-basic-intro-to-dom/2-notification/src/index.js
@@ -1,3 +1,36 @@
 export default class NotificationMessage {
+  constructor(message, options) {
+    this.message = message;
+    this.duration = options.duration;
+    this.type = options.type;
+    this.initEventListeners();
+  }
 
+  initEventListeners() {
+    this.timeOutId = setTimeout(() => this.remove(), this.duration);
+  }
+
+  show() {
+    const elementHTML = `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+    </div>`;
+    const btn = document.getElementById('btn1');
+    btn.insertAdjacentHTML('afterend', elementHTML);
+    this.element = document.querySelector('.notification');
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timeOutId);
+  }
 }
